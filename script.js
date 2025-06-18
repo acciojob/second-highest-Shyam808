272c7885624217cc7d63fcc58cbf59ca74b33ec9
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-18 21:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
 
+  let first = -Infinity;
+  let second = -Infinity;
 
-	function secondHighest(arr) {
-		// Convert all elements to numbers (prompt returns strings)
-		arr = arr.map(Number);
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num < first && num > second) {
+      second = num;
+    }
+  }
 
-		// Remove duplicates
-		let uniqueArr = [...new Set(arr)];
-
-		// If not enough distinct elements, return -Infinity
-		if (uniqueArr.length < 2) return -Infinity;
-
-		// Sort in descending order and return second element
-		uniqueArr.sort((a, b) => b - a);
-		return uniqueArr[1];
-	}
-
-	function Main() {
-		var n = prompt("Enter the number of elements");
-		var arr = [];
-		for (var i = 0; i < n; i++) {
-			arr[i] = prompt("Enter element " + (i + 1));
-		}
-		alert(secondHighest(arr));
-	}
-	Main();
-
+  return second;
+}
